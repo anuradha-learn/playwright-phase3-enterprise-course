7073031e9ebfbc2fb4c6176bd8f2306ed713df71
Anuradha <anuradha.learn@gmail.com>
2026-09-09 11:10:20 +0400

    4 workers crossbrowser

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ if (!process.env.API_BASE_URL) throw new Error('API_BASE_URL is not set in .env'
 
 
 export default defineConfig({ 
-    workers: process.env.CI ? 1 : 1,
+    workers: process.env.CI ? 4 : 1,
     fullyParallel: false,
     timeout: 40 * 1000,
     expect: { timeout: 40 * 1000 },
